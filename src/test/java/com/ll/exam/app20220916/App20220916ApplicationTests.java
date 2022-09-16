a39a654105337467e2e0078f150bf22d64f2ad4e
SangHun <shzero211@naver.com>
2022-09-16 09:43:23 +0900

    first

---
+++ src/test/java/com/ll/exam/app20220916/App20220916ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app20220916;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20220916ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
