b349c68e5c693009b640952691be9a668d4cff35
SangHun <shzero211@naver.com>
2022-09-19 11:37:24 +0900

    feat:추가 테스트 설정

--- src/test/java/com/ll/exam/app20220916/App20220916ApplicationTests.java
+++ src/test/java/com/ll/exam/app20220916/App20220916ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.exam.app20220916;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class App20220916ApplicationTests {
 
 	@Test
